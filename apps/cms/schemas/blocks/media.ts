71e5d9c71c8fa4bd00dc6f66bf5c5980b7459c7f
Nathan Nye <nathan@nye.design>
2025-10-22 15:44:22 +0200

    media

--- apps/cms/schemas/blocks/media.ts
+++ apps/cms/schemas/blocks/media.ts
@@ -1,27 +1,42 @@
+import { MdImage, MdPlayCircle } from "react-icons/md";
 import MediaSelector from "../../components/MediaSelector/MediaSelector";
 
 export default {
-  name: 'media',
-  components: {
-    input: MediaSelector,
-  },
-  type: 'object',
-  fields: [
-    {
-      name: 'mediaType', 
-      initialValue: 'image',
-      type: 'string',
-      options: {
-        list: ['image', 'video'],
-      },
-    },
-    {
-      name: 'image',
-      type: 'image',
-    },
-    {
-      name: 'video',
-      type: 'mux.video',
-    }
-  ],
-}
+	name: "media",
+	components: {
+		input: MediaSelector,
+	},
+	preview: {
+		select: {
+			video: "video",
+			mediaType: "mediaType",
+			image: "image",
+		},
+		prepare({ image, mediaType }) {
+			return {
+				title: mediaType === "image" ? "Image" : "Video",
+				media: mediaType === "image" && image,
+				icon: mediaType === "image" ? MdImage : MdPlayCircle,
+			};
+		},
+	},
+	type: "object",
+	fields: [
+		{
+			name: "mediaType",
+			initialValue: "image",
+			type: "string",
+			options: {
+				list: ["image", "video"],
+			},
+		},
+		{
+			name: "image",
+			type: "image",
+		},
+		{
+			name: "video",
+			type: "mux.video",
+		},
+	],
+};
