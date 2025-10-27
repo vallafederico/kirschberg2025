df372516a44a1522d46556318396470e3d542c4f
Nathan Nye <nathan@nye.design>
2025-10-27 15:02:55 +0100

    spacing

--- apps/web/src/components/slices/Quote.tsx
+++ apps/web/src/components/slices/Quote.tsx
@@ -11,7 +11,7 @@ export default function Quote({
 		<section class="pt-64 pb-98 lg:pl-grid-2 lg:pr-50">
 			<span class="text-37 font-sans">“</span>
 			<Show when={quote}>
-				<blockquote class="text-37 font-display">{quote}</blockquote>
+				<blockquote class="text-32 lg:text-37 font-display">{quote}</blockquote>
 			</Show>
 			<Show when={author}>
 				<p class="text-18 font-sans opacity-80 mt-12 font-semibold">{author}</p>
