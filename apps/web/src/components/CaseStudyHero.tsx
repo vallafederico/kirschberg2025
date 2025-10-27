198f5b265e8873d853fcc6b4350db442a4235f34
Nathan Nye <nathan@nye.design>
2025-10-27 15:04:33 +0100

    w

--- apps/web/src/components/CaseStudyHero.tsx
+++ apps/web/src/components/CaseStudyHero.tsx
@@ -23,7 +23,7 @@ export default function CaseStudyHero({
 					{...(featuredMedia?.[0] || {})}
 				/>
 			</div>
-			<div class="lg:w-[47%] max-lg:px-margin-1 lg:ml-grid-2">
+			<div class="lg:w-[50%] max-lg:px-margin-1 lg:ml-grid-2">
 				<h1 id="case-title" class="text-32 font-display font-medium mt-44">
 					{title}
 				</h1>
