2de6ebd3389e4dc733dbaaa7509ccc0d5fea4a7b
Nathan Nye <nathan@nye.design>
2025-10-27 01:14:30 +0100

    nvm

--- packages/seo/index.ts
+++ packages/seo/index.ts
@@ -1,2 +1,2 @@
-export { default as SchemaMarkup } from "./components/SchemaMarkup";
 export { default as SanityMeta } from "./components/SanityMeta";
+export { default as SchemaMarkup } from "./components/SchemaMarkup";
