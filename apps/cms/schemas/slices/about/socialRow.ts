cb0860ec268cc4d02f9dcd82a3bd94a362719476
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-11-24 22:37:29 +0100

    on bg on buttons

--- apps/cms/schemas/slices/about/socialRow.ts
+++ apps/cms/schemas/slices/about/socialRow.ts
@@ -1,31 +1,31 @@
-import { MdLink } from "react-icons/md";
-import { createPreview } from "../../../utils/preview";
+import {MdLink} from 'react-icons/md'
+import {createPreview} from '../../../utils/preview'
 
 export default {
-	name: "socialRow",
-	preview: createPreview("{Social Row}"),
-	type: "object",
-	fields: [
-		{
-			name: "items",
-			type: "array",
-			of: [
-				{
-					type: "object",
-					icon: MdLink,
-					preview: createPreview("name", "url"),
-					fields: [
-						{
-							name: "name",
-							type: "string",
-						},
-						{
-							name: "url",
-							type: "string",
-						},
-					],
-				},
-			],
-		},
-	],
-};
+  name: 'socialRow',
+  preview: createPreview('{Social Row}'),
+  type: 'object',
+  fields: [
+    {
+      name: 'items',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          icon: MdLink,
+          preview: createPreview('name', 'url'),
+          fields: [
+            {
+              name: 'name',
+              type: 'string',
+            },
+            {
+              name: 'url',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
--- apps/web/src/components/slices/socialRow.tsx
+++ apps/web/src/components/slices/socialRow.tsx
@@ -2,25 +2,25 @@ import { For } from "solid-js";
 import Button from "../Button";
 
 export default function SocialRow({ items }: { items: any[] }) {
-	return (
-		<div>
-			<h2 class="text-24 font-bold mb-40">Social</h2>
-			<ul class="flex gap-x-10">
-				<For each={items}>
-					{(item) => (
-						<li class="w-full">
-							<Button
-								class="w-full"
-								link={{
-									linkType: "external",
-									url: item.url,
-									label: item.name,
-								}}
-							/>
-						</li>
-					)}
-				</For>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <h2 class="text-24 mb-40 font-bold">Social</h2>
+      <ul class="flex gap-x-10">
+        <For each={items}>
+          {(item) => (
+            <li class="w-full">
+              <Button
+                class="w-full bg-transparent"
+                link={{
+                  linkType: "external",
+                  url: item.url,
+                  label: item.name,
+                }}
+              />
+            </li>
+          )}
+        </For>
+      </ul>
+    </div>
+  );
 }
