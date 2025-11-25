f2a2161eab98e40623d2f2d5f956995ad807fe73
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-11-25 15:41:45 +0100

    add clickk  to exit

--- apps/web/src/routes/case-study.tsx
+++ apps/web/src/routes/case-study.tsx
@@ -1,49 +1,60 @@
 import { getDocumentBySlug, SanityComponents, SanityPage } from "@local/sanity";
 import { SanityMeta } from "@local/seo";
-import { createAsync, query, type RouteSectionProps } from "@solidjs/router";
+import {
+  createAsync,
+  query,
+  useNavigate,
+  type RouteSectionProps,
+} from "@solidjs/router";
 import CaseStudyHero from "~/components/CaseStudyHero";
 import CaseStudyIntro from "~/components/CaseStudyIntro";
 import CaseStudySubnav from "~/components/CaseStudySubnav";
 import { SLICE_LIST } from "~/components/slices";
 
 const getCaseStudyData = query(async (slug: string) => {
-	"use server";
+  "use server";
 
-	return await getDocumentBySlug("case-study", slug);
+  return await getDocumentBySlug("case-study", slug);
 }, "case-study");
 
 export default function CaseStudy(props: RouteSectionProps) {
-	const slug = props.params.slug;
-	const data = createAsync(() => getCaseStudyData(slug));
+  const slug = props.params.slug;
+  const data = createAsync(() => getCaseStudyData(slug));
 
-	return (
-		<SanityPage
-			element="dialog"
-			class=" lg:pt-95 lg:pb-117 relative z-2 bg-[black]/30 size-full"
-			fetcher={data}
-			open={true}
-			aria-modal={true}
-			aria-labelledby="case-title"
-		>
-			{(caseStudy: any) => {
-				return (
-					<>
-						<SanityMeta pageData={caseStudy} />
-						<article class="lg:w-920 z-2 relative lg:rounded-xxl min-h-[140vh] bg-primary text-inverted mx-auto lg:pb-86 lg:pt-54 lg:px-64 relative pb-20">
-							<CaseStudyHero {...caseStudy} />
-							<CaseStudyIntro {...caseStudy} />
-							<div class="flex max-lg:px-margin-1 flex-col gap-32">
-								<SanityComponents
-									components={caseStudy.slices}
-									componentList={SLICE_LIST}
-								/>
-							</div>
-						</article>
-						<CaseStudySubnav link={caseStudy.liveLink} />
-						<div class="fixed z-1 inset-0 size-full bg-[black]/30 lg:backdrop-blur-[4px]"></div>
-					</>
-				);
-			}}
-		</SanityPage>
-	);
+  const navigate = useNavigate();
+
+  return (
+    <SanityPage
+      element="dialog"
+      class="relative z-2 size-full bg-[black]/30 lg:pt-95 lg:pb-117"
+      fetcher={data}
+      open={true}
+      aria-modal={true}
+      aria-labelledby="case-title"
+    >
+      {(caseStudy: any) => {
+        return (
+          <>
+            <SanityMeta pageData={caseStudy} />
+
+            <article class="lg:rounded-xxl bg-primary text-inverted relative z-2 mx-auto min-h-[140vh] pb-20 lg:w-920 lg:px-64 lg:pt-54 lg:pb-86">
+              <CaseStudyHero {...caseStudy} />
+              <CaseStudyIntro {...caseStudy} />
+              <div class="max-lg:px-margin-1 flex flex-col gap-32">
+                <SanityComponents
+                  components={caseStudy.slices}
+                  componentList={SLICE_LIST}
+                />
+              </div>
+            </article>
+            <CaseStudySubnav link={caseStudy.liveLink} />
+            <div
+              onClick={() => navigate("/")}
+              class="fixed inset-0 z-1 size-full bg-[black]/30 lg:backdrop-blur-xs"
+            ></div>
+          </>
+        );
+      }}
+    </SanityPage>
+  );
 }
