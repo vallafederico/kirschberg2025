61e8eb04f377e1b4ad943817d6171ee421312ba9
Nathan Nye <nathan@nye.design>
2025-11-25 08:42:52 -0700

    test revert about route

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,40 +1,43 @@
 import "./app.css";
 import { Link, MetaProvider } from "@solidjs/meta";
 import { Route, Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import { useViewport } from "~/lib/hooks/useViewport";
-import { FileRoutes } from "@solidjs/start/router";
 import Layout from "./components/Layout";
 import ThemeManager from "./components/ThemeManager";
 import Home from "./routes/(home)";
+import AboutPage from "./routes/about";
 import CaseStudy from "./routes/case-study";
 
 export default function App() {
-  useViewport();
+	useViewport();
 
-  return (
-    <Router
-      root={(props) => (
-        <MetaProvider>
-          <ThemeManager />
-          <Link rel="robots" type="text/plain" href="/api/robots.txt" />
+	return (
+		<Router
+			root={(props) => (
+				<MetaProvider>
+					<ThemeManager />
+					<Link rel="robots" type="text/plain" href="/api/robots.txt" />
 
-          <Suspense>
-            <Layout>{props.children}</Layout>
-          </Suspense>
-        </MetaProvider>
-      )}
-    >
-      <Route path="/" component={Home}>
-        <Route path="/" component={Home} />
-        <Route path="/case/:slug" component={CaseStudy} />
-      </Route>
+					<Suspense>
+						<Layout>{props.children}</Layout>
+					</Suspense>
+				</MetaProvider>
+			)}
+		>
+			<Route path="/" component={Home}>
+				<Route path="/" component={Home} />
+				<Route path="/case/:slug" component={CaseStudy} />
+			</Route>
 
-      <Suspense>
+			<Route path="/cms" component={AboutPage} />
+
+			{/* <Suspense>
         <FileRoutes />
-      </Suspense>
+      </Suspense> */}
 
-      {/* <Route path="/about" component={AboutPage} /> */}
-    </Router>
-  );
+			{/* <Route path="/about" component={AboutPage} /> */}
+		</Router>
+	);
 }
